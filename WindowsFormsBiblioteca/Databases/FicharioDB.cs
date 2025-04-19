afbea7f065736b6cc89209cb88671575ba4156df
Carlos4415 <carlos.soares4415@gmail.com>
2025-04-18 22:26:03 -0300

    Mais comandos SQL

--- WindowsFormsBiblioteca/Databases/FicharioDB.cs
+++ WindowsFormsBiblioteca/Databases/FicharioDB.cs
@@ -20,6 +20,7 @@ namespace WindowsFormsBiblioteca.Databases
             try
             {
                 db = new LocalDBClass();
+
                 tabela = Tabela;
                 mensagem = "Conexão com a Tabela criada com sucesso.";
             }
@@ -39,7 +40,6 @@ namespace WindowsFormsBiblioteca.Databases
                 // INSERT INTO CLIENTE (ID, JSON) VALUES ('000001', '{...}')
 
                 var SQL = "INSERT INTO " + tabela + " (Id, JSON) VALUES ('" + Id + "', '" + JsonUnit + "')";
-
                 db.SQLCommand(SQL);
 
                 status = true;
@@ -51,5 +51,140 @@ namespace WindowsFormsBiblioteca.Databases
                 mensagem = "Conexão com o Fichario com erro: " + ex.Message;
             }
         }
+
+        public string Buscar(string Id)
+        {
+            status = true;
+
+            try
+            {
+                // SELECT ID, JSON CLIENTE WHERE ID = '000010'
+
+                var SQL = "SELECT Id, JSON FROM " + tabela + " WHERE Id = '" + Id + "'";
+                var dt = db.SQLQuery(SQL);
+
+                if (dt.Rows.Count > 0)
+                {
+                    string conteudo = dt.Rows[0]["JSON"].ToString();
+                    status = true;
+
+                    return conteudo;
+                }
+                else
+                {
+                    status = false;
+                    mensagem = "Identificador não existe: " + Id;
+                }
+            }
+            catch (Exception ex) 
+            { 
+                status = false;
+                mensagem = "Erro ao buscar o conteúdo do identificador: " + ex.Message;
+            }
+
+            return "";
+        }
+
+        public List<string> BuscarTodos()
+        {
+            status = true;
+            List<string> List = new List<string>();
+
+            try
+            {
+                // SELECT ID, JSON FROM CLIENTE
+
+                var SQL = "SELECT Id, JSON FROM " + tabela;
+                var dt = db.SQLQuery(SQL);
+
+                if (dt.Rows.Count > 0)
+                {
+                    for (int i = 0; i < dt.Rows.Count - 1; i++)
+                    {
+                        string conteudo = dt.Rows[i]["JSON"].ToString();
+
+                        List.Add(conteudo);
+                    }
+
+                    return List;
+                }
+                else
+                {
+                    status = false;
+                    mensagem = "Não existe clientes na base de dados.";
+                }
+            }
+            catch (Exception ex) 
+            { 
+                status = false;
+                mensagem = "Erro ao buscar o conteúdo do identificador: " + ex.Message;
+            }
+
+            return List;
+        }
+
+        public void Apagar(string Id)
+        {
+            status = true;
+
+            try
+            {
+                var SQL = "SELECT Id, JSON FROM " + tabela  + " WHERE Id = '" + Id + "'";
+                var dt = db.SQLQuery(SQL);
+
+                if (dt.Rows.Count > 0)
+                {
+                    // DELETE FROM CLIENTE WHERE ID = '000010'
+
+                    SQL = "DELETE FROM '" + tabela + "' WHERE Id = '" + Id + "'";
+                    db.SQLCommand(SQL);
+
+                    status = true;
+                    mensagem = "Exclusão efetuada com sucesso. Identificador: " + Id;
+                }
+                else
+                {
+                    status = false;
+                    mensagem = "Exclusão não permitida porque o identificador não existe: " + Id;
+                }
+                
+            }
+            catch (Exception ex) 
+            { 
+                status = false;
+                mensagem = "Conexão com o Fichario com erro: " + ex.Message;
+            }
+        }
+
+        public void Alterar(string Id, string jsonUnit)
+        {
+            status = true;
+
+            try
+            {
+                var SQL = "SELECT Id, JSON FROM '" + tabela + " WHERE Id = '" + Id + "'";
+                var dt = db.SQLQuery(SQL);
+
+                if (dt.Rows.Count > 0) 
+                {
+                    // UPDATE CLIENTE SET JSON = '{...}' WHERE ID = '000010'
+
+                    SQL = "UPDATE '" + tabela + "' SET JSON = '" + jsonUnit + "' WHERE Id = '" + Id + "'";
+                    db.SQLCommand(SQL);
+
+                    status = true;
+                    mensagem = "Alteração efetuada com sucesso. Identificador: " + Id;
+                }
+                else
+                {
+                    status = false;
+                    mensagem = "Alteração não permitida porque o identificador não existe: " + Id;
+                }
+            }
+            catch (Exception ex) 
+            { 
+                status = false;
+                mensagem = "Conexão com o Fichario com erro: " + ex.Message;
+            }
     }
 }
